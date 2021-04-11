5b68f97ad69f116375e1e42a3d191274e6a2e6e8
fhrryDeveloper <fhrrydeveloper@gmail.com>
2021-04-11 15:15:27 +0300

    Ionic and Vue starter Project Commit

---
+++ src/views/Tab1.vue
@@ -0,0 +1,28 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Ionic-Vue</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <h1>Dynamic Import Icon</h1>
+      <ion-icon :icon="heart"></ion-icon>
+      <h1>Global Import Icon</h1>
+      <ion-icon icon="heart"></ion-icon>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import { heart } from 'ionicons/icons';
+
+export default  {
+  name: 'Tab1',
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage },
+  setup() {
+    return { heart }
+  }
+}
+</script>
